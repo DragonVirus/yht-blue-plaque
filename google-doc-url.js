d15fe4ff443347bafbacdba4c3ebd9c3b56ab797
DragonVirus <limonster.li@gmail.com>
2020-05-10 22:40:12 +0630

    Change Google Docs URL

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1GcdCig2vij6sQV6A36NSN_3V3jQS5nxmHCJIcPyQgAY/';
